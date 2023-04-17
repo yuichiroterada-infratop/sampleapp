7b01d24e84f298297510790adfe0f9903a7b4d9d
EC2 Default User <ec2-user@ip-172-31-13-223.ap-northeast-1.compute.internal>
2023-04-17 06:54:41 +0000

    [Fin]~active storage

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,29 @@
+class ListsController < ApplicationController
+  def new
+    @list = List.new
+  end
+  
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to list_path(list.id)
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+  end
+  
+  private
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
+  end
+  
+end
